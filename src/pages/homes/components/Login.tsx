078a9b403bc38118fa96ac071febb26ebfeb1fea
minhnghialk <nghia13031995@gmail.com>
2023-10-16 17:58:53 +0700

    login

--- src/pages/homes/components/Login.tsx
+++ src/pages/homes/components/Login.tsx
@@ -1,37 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 //facebook
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import FacebookAuth from '../../../module/facebook/Facebook';
-import Facebook from '../../../module/facebook/Facebook';
-import UserProfile from '../../../module/facebook/UserProfile';
-import FacebookLoginButton from '../../../module/facebook/Facebook';
-import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import FacebookAuth from "../../../module/facebook/Facebook";
+// import Facebook from "../../../module/facebook/Facebook";
+import UserProfile from "../../../module/facebook/UserProfile";
+import { useState } from "react";
 
-
-
-import React from 'react'
+import React from "react";
+import LoginForm from "./loginForm/LoginForm";
 
 export default function Login() {
   //facebook
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userData, setUserData] = useState({ name: '', email: '', picture: { data: { url: '' } } });
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    picture: { data: { url: "" } },
+  });
 
   const handleLogin = (response: any) => {
     setIsLoggedIn(true);
     setUserData(response);
   };
 
-
   return (
-    <div className='container'>
+    <div className="container">
       {/* facebook */}
-      <div>
-        {isLoggedIn ? (
-          <UserProfile user={userData} />
-        ) : (
-          <FacebookLoginButton onLogin={handleLogin} />
-        )}
-      </div>
-
+      <div>{isLoggedIn ? <UserProfile user={userData} /> : <LoginForm />}</div>
     </div>
-  )
+  );
 }

6de1b7598fb91a8b8458dc81b5703fe801b391cd
apple <apple@applenoMacBook-Air.local>
2023-10-17 08:58:33 +0700

    ui

--- src/pages/admin/forecast/Forecast.tsx
+++ src/pages/admin/forecast/Forecast.tsx
@@ -1,7 +1,205 @@
-import React from 'react'
+
+
+import { Table } from 'antd';
+import type { ColumnsType, TableProps } from 'antd/es/table';
+import React, { useState } from 'react';
+import "../category/category.scss"
+import { Button, Form, Modal } from 'react-bootstrap';
+
+interface DataType {
+    key: React.Key;
+    name: string;
+    age: number;
+    address: string;
+}
+
+const columns: ColumnsType<DataType> = [
+    {
+        title: 'Index',
+        dataIndex: 'age',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.age - b.age,
+    },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        filters: [
+            {
+                text: 'Joe',
+                value: 'Joe',
+            },
+            {
+                text: 'Jim',
+                value: 'Jim',
+            },
+            {
+                text: 'Submenu',
+                value: 'Submenu',
+                children: [
+                    {
+                        text: 'Green',
+                        value: 'Green',
+                    },
+                    {
+                        text: 'Black',
+                        value: 'Black',
+                    },
+                ],
+            },
+        ],
+        // specify the condition of filtering result
+        // here is that finding the name started with `value`
+        onFilter: (value: string, record) => record.name.indexOf(value) === 0,
+        sorter: (a, b) => a.name.length - b.name.length,
+        sortDirections: ['descend'],
+    },
+
+    {
+        title: 'Lat',
+        dataIndex: 'address',
+        filters: [
+            {
+                text: 'London',
+                value: 'London',
+            },
+            {
+                text: 'New York',
+                value: 'New York',
+            },
+        ],
+        onFilter: (value: string, record) => record.address.indexOf(value) === 0,
+    },
+    {
+        title: "Lag"
+    },
+    {
+        title: "Level"
+    },
+    {
+        title: "Place"
+    },
+    {
+        title: "Size"
+    },
+ 
+    {
+        title: "Actions",
+        render: (text, record) => (
+            <div>
+                <button type="button" className="btn btn-primary" onClick={() => handleEdit(record)} style={{ width: "100px", marginLeft: "40px", marginRight: "20px" }}>Edit</button>
+                <button type="button" className="btn btn-danger" onClick={() => handleDelete(record)} style={{marginRight:"10px"}}>Delete</button>
+            </div>
+        ),
+
+    }
+];
+
+const data = [
+    {
+        key: '1',
+        name: 'aohn Brown',
+        age: 32,
+        address: 'New York No. 1 Lake Park',
+    },
+    {
+        key: '2',
+        name: 'bim Green',
+        age: 42,
+        address: 'London No. 1 Lake Park',
+    },
+    {
+        key: '3',
+        name: 'Joe Black',
+        age: 32,
+        address: 'Sidney No. 1 Lake Park',
+    },
+    {
+        key: '4',
+        name: 'cim Red',
+        age: 32,
+        address: 'London No. 2 Lake Park',
+    },
+];
+
+const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
+    console.log('params', pagination, filters, sorter, extra);
+};
 
 export default function Forecast() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
     return (
-        <div className='component'>Forecast</div>
+        <div className='component'>
+            <div style={{ display: "flex", marginLeft: "300px", marginTop: "30px", marginBottom: "30px" }}>
+                <h4 style={{}}>List Earthquake</h4>
+                <Button variant="primary" style={{ marginLeft: "500px" }} onClick={handleShow}>New Earthquake</Button>
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    placeholder="name@example.com"
+                                    autoFocus
+                                />
+                            </Form.Group>
+                            <Form.Group
+                                className="mb-3"
+                                controlId="exampleForm.ControlTextarea1"
+                            >
+                                <Form.Label>Example textarea</Form.Label>
+                                <Form.Control as="textarea" rows={3} />
+                            </Form.Group>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+            <div className="row" style={{ marginBottom: "20px" }}>
+                <div className="col-md-5 mx-auto">
+                    <div className="input-group">
+                        <input
+                            className="form-control border-end-0 border rounded-pill"
+                            type="search"
+                            placeholder='Tìm kiếm theo tên'
+                            id="example-search-input"
+                        />
+                        <span className="input-group-append">
+                            <button
+                                className="btn btn-outline-secondary bg-white border-bottom-0 border rounded-pill ms-n5"
+                                type="button"
+                            >
+                                <i className="fa fa-search" />
+                            </button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+            <Table columns={columns} dataSource={data} onChange={onChange} />
+        </div>
     )
 }
+function handleEdit(record: DataType) {
+    throw new Error('Function not implemented.');
+}
+
+function handleDelete(record: DataType) {
+    throw new Error('Function not implemented.');
+}
+
